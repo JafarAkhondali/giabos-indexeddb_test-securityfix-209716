732986e3e30de707828c0b0883bffc7b50919529
giabos <gianni.boscarino@gmail.com>
2014-11-13 22:26:40 +0100

    a

--- ratchet-v2.0.2/js/app.js
+++ ratchet-v2.0.2/js/app.js
@@ -9,12 +9,31 @@
 			vm.list = ["AAA", "BBB", "CCC"];
 		});
 	}
+    function indexPage () {
+        var vm = new EZ(document.documentElement);
+		vm._do(function () {
+            vm.settings={};
+	        vm.settings.name = localStorage.getItem('settings.name');	
+	        vm.settings.email = localStorage.getItem('settings.email');	
+		});
+        vm._listen('settings.name', function () {
+            localStorage.setItem('settings.name', vm.settings.name);
+        });
+        vm._listen('settings.email', function () {
+            localStorage.setItem('settings.email', vm.settings.email);
+        });
+    }
+
+    indexPage();
 
 	// http://stackoverflow.com/questions/23145097/execute-custom-script-after-page-loaded-with-ratchet-push-js
 	window.addEventListener('push', function (evt) {
 		console.log("vm setup ", evt.detail.state.url);
 		if (/\/other.html$/.test(evt.detail.state.url)) {
 			otherPage();
+		}
+		if (/\/index.html$/.test(evt.detail.state.url)) {
+			indexPage();
 		}
 	});
 
